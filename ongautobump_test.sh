86c7db2e279363548cff60b3e58876eff1dd4eb2
In-The-Shad0ws <InTheShad0ws@mydhe.com>
2025-07-21 20:28:50 -0500

    Fixes

--- ongautobump_test.sh
+++ ongautobump_test.sh
@@ -10,4 +10,4 @@ source $AUTOBUMPDIR/.venv/bin/activate
 
 GSHEETID=`cat $AUTOBUMPDIR/gsheet_test.id`
 
-tail -n 100  -f /home/ongbots/ongwatch/bump.log"   | ./ongautobump.py --gsheets-credentials-file $AUTOBUMPDIR/gsheets_credentials.json --gsheets-id $SHEETID
+tail -n 100  -f /home/ongbots/ongwatch/bump.log   | ./ongautobump.py --gsheets-credentials-file $AUTOBUMPDIR/gsheets_credentials.json --gsheets-id $SHEETID
