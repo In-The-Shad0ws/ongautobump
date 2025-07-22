4975f675765252af8dfd422e60167a97a1f599fc
In-The-Shad0ws <InTheShad0ws@mydhe.com>
2025-07-21 20:33:04 -0500

    Prod script

--- ongautobump_prod.sh
+++ ongautobump_prod.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 #
 # Runs the Ong Auto Bump Logger to the test
 AUTOBUMPDIR=/home/ongbots/ongautobump
@@ -7,6 +7,6 @@ pip3 install -r requirements.txt
 
 source $AUTOBUMPDIR/.venv/bin/activate
 
-GSHEETID=`cat $AUTOBUMPDIR/gsheet_test.id`
+GSHEETID=`cat $AUTOBUMPDIR/gsheet_prod.id`
 
 tail -n 100  -f /home/ongbots/ongwatch/bump.log   | ./ongautobump.py --gsheets-credentials-file $AUTOBUMPDIR/gsheets_credentials.json --gsheets-id $GSHEETID
